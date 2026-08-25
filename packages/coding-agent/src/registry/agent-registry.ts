641-git641/oh-my-pi

fix(hub): prevented stale agent refs from blocking wait
Mirrored registry status from pre-wire session run-state transitions and required live session corroboration before a peer can sustain bare hub waits.

Fixes #8634

packages/coding-agent/src/registry/agent-registry.ts
@@ -9,6 +9,7 @@
  * revival) and are only removed on explicit release/teardown.
  */
 
+import { logger } from "@oh-my-pi/pi-utils";
 import type { AgentSession } from "../session/agent-session";
 import { oneLineLabel } from "../task/types";
 
@@ -135,6 +136,11 @@ export class AgentRegistry {
 		return expected === undefined || ref === expected || ref.session === expected;
 	}
 
+	#rejectStatusUpdate(id: string, status: AgentStatus, reason: string): false {
+		logger.debug("Agent registry status update rejected", { id, status, reason });
+		return false;
+	}
+
 	register(input: RegisterInput): AgentRef {
 		const now = Date.now();
 		const ref: AgentRef = {
@@ -181,10 +187,15 @@ export class AgentRegistry {
 
 	setStatus(id: string, status: AgentStatus, expected?: AgentRefExpectation): boolean {
 		const ref = this.#refs.get(id);
-		if (!ref || !this.#matchesExpected(ref, expected)) return false;
+		if (!ref) return this.#rejectStatusUpdate(id, status, "missing-ref");
+		if (!this.#matchesExpected(ref, expected)) {
+			return this.#rejectStatusUpdate(id, status, "session-ownership-changed");
+		}
 		// `aborted` is terminal: delayed progress/revival work from the killed
 		// generation must never transition the tombstone back to a live status.
-		if (ref.status === "aborted") return status === "aborted";
+		if (ref.status === "aborted") {
+			return status === "aborted" || this.#rejectStatusUpdate(id, status, "aborted-is-terminal");
+		}
 		if (ref.status === status) return true;
 		ref.status = status;
 		// Activity describes current work; it is meaningless once the agent
@@ -268,6 +279,20 @@ export class AgentRegistry {
 		return this.list().filter(
 			ref => ref.id !== id && ref.kind !== "advisor" && (ref.status === "running" || ref.status === "idle"),
 		);
+	}
+
+	/** Whether a ref's claimed running state is corroborated by its attached live session. */
+	isRunning(ref: AgentRef): boolean {
+		if (ref.status !== "running") return false;
+		return ref.session?.isStreaming === true;
+	}
+
+	/** Mirror a session's authoritative run-state notifications into its owned registry ref. */
+	syncSessionStatus(id: string, session: AgentSession): () => void {
+		const unsubscribe = session.subscribeRunState(status => {
+			this.setStatus(id, status, session);
+		});
+		return unsubscribe;
 	}
 
 	onChange(listener: RegistryListener): () => void {

N3thunt3r69/oh-my-soup

packages/coding-agent/src/registry/agent-registry.ts
@@ -9,6 +9,7 @@
  * revival) and are only removed on explicit release/teardown.
  */
 
+import { logger } from "@oh-my-soup/pi-utils";
 import type { AgentSession } from "../session/agent-session";
 import { oneLineLabel } from "../task/types";
 
@@ -135,6 +136,11 @@ export class AgentRegistry {
 		return expected === undefined || ref === expected || ref.session === expected;
 	}
 
+	#rejectStatusUpdate(id: string, status: AgentStatus, reason: string): false {
+		logger.debug("Agent registry status update rejected", { id, status, reason });
+		return false;
+	}
+
 	register(input: RegisterInput): AgentRef {
 		const now = Date.now();
 		const ref: AgentRef = {
@@ -181,10 +187,15 @@ export class AgentRegistry {
 
 	setStatus(id: string, status: AgentStatus, expected?: AgentRefExpectation): boolean {
 		const ref = this.#refs.get(id);
-		if (!ref || !this.#matchesExpected(ref, expected)) return false;
+		if (!ref) return this.#rejectStatusUpdate(id, status, "missing-ref");
+		if (!this.#matchesExpected(ref, expected)) {
+			return this.#rejectStatusUpdate(id, status, "session-ownership-changed");
+		}
 		// `aborted` is terminal: delayed progress/revival work from the killed
 		// generation must never transition the tombstone back to a live status.
-		if (ref.status === "aborted") return status === "aborted";
+		if (ref.status === "aborted") {
+			return status === "aborted" || this.#rejectStatusUpdate(id, status, "aborted-is-terminal");
+		}
 		if (ref.status === status) return true;
 		ref.status = status;
 		// Activity describes current work; it is meaningless once the agent
@@ -268,6 +279,20 @@ export class AgentRegistry {
 		return this.list().filter(
 			ref => ref.id !== id && ref.kind !== "advisor" && (ref.status === "running" || ref.status === "idle"),
 		);
+	}
+
+	/** Whether a ref's claimed running state is corroborated by its attached live session. */
+	isRunning(ref: AgentRef): boolean {
+		if (ref.status !== "running") return false;
+		return ref.session?.isStreaming === true;
+	}
+
+	/** Mirror a session's authoritative run-state notifications into its owned registry ref. */
+	syncSessionStatus(id: string, session: AgentSession): () => void {
+		const unsubscribe = session.subscribeRunState(status => {
+			this.setStatus(id, status, session);
+		});
+		return unsubscribe;
 	}
 
 	onChange(listener: RegistryListener): () => void {